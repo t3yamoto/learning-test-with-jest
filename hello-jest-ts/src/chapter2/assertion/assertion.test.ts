43d8d69b738dbecda500ecbb42d8b7bf4f8459f7
t3yamoto <3yamoto.dev@gmail.com>
2023-04-15 23:05:37 +0900

    P38

--- hello-jest-ts/src/chapter2/assertion/assertion.test.ts
+++ hello-jest-ts/src/chapter2/assertion/assertion.test.ts
@@ -7,3 +7,29 @@ test('testを使用してテストケースを作成する', () => {
 it('itを使用してテストケースを作成する', () => {
     expect(true).toBe(true)
 })
+
+
+const numberValue = 0
+const stringValue = '文字列'
+const booleanValue = true
+
+// toBeでプリミティブな値を評価
+test('evaluates as equal for all the same primitive values when using the toBe function', () => {
+    expect(numberValue).toBe(0)
+    expect(stringValue).toBe('文字列')
+    expect(booleanValue).toBe(true)
+})
+
+// toEqualでプリミティブな値を評価
+test('evaluates as equal for all the same primitive values when using the toEqual function', () => {
+    expect(numberValue).toEqual(0)
+    expect(stringValue).toEqual('文字列')
+    expect(booleanValue).toEqual(true)
+})
+
+// toStrictEqualでプリミティブな値を評価
+test('evaluates as equal for all the same primitive values when using the toStrictEqual function', () => {
+    expect(numberValue).toStrictEqual(0)
+    expect(stringValue).toStrictEqual('文字列')
+    expect(booleanValue).toStrictEqual(true)
+})

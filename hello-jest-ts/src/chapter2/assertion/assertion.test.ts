49d9f0bbdb5d5881bbc9a378ca8b84d68f735329
t3yamoto <3yamoto.dev@gmail.com>
2023-04-15 22:36:41 +0900

    P35

---
+++ hello-jest-ts/src/chapter2/assertion/assertion.test.ts
@@ -0,0 +1,9 @@
+test('testを使用してテストケースを作成する', () => {
+    const result = true
+    const expected = true
+    expect(result).toBe(expected)
+})
+
+it('itを使用してテストケースを作成する', () => {
+    expect(true).toBe(true)
+})

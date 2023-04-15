94dbf9d5b8ed2f0ef7a9ad0372f6058ff00849b5
t3yamoto <3yamoto.dev@gmail.com>
2023-04-15 22:46:54 +0900

    P36

---
+++ hello-jest-ts/src/chapter2/assertion/equalityOperators.js
@@ -0,0 +1,14 @@
+console.log('2' == 2)
+console.log(true == 1)
+
+console.log('2' === 2)
+console.log(true === 1)
+
+console.log(NaN == NaN)
+console.log(+0 == -0)
+
+console.log(NaN === NaN)
+console.log(+0 === -0)
+
+console.log(Object.is(NaN, NaN))
+console.log(Object.is(+0, -0))

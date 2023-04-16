44b13060d0cce9ffc442a3416f4d15362fda4e14
t3yamoto <3yamoto.dev@gmail.com>
2023-04-16 22:42:51 +0900

    P41

--- hello-jest-ts/src/chapter2/assertion/assertion.test.ts
+++ hello-jest-ts/src/chapter2/assertion/assertion.test.ts
@@ -33,3 +33,65 @@ test('evaluates as equal for all the same primitive values when using the toStri
     expect(stringValue).toStrictEqual('文字列')
     expect(booleanValue).toStrictEqual(true)
 })
+
+type CanType = {
+    flavor: string
+    ounces: number
+}
+
+const can1: CanType = {
+    flavor: 'grapefruit',
+    ounces: 12
+}
+
+const can2: CanType = {
+    flavor: 'grapefruit',
+    ounces: 12
+}
+
+const can3: CanType = can2
+
+class Can {
+    flavor: string
+    ounces: number
+
+    constructor({ flavor, ounces }: CanType){
+        this.flavor = flavor
+        this.ounces = ounces
+    }
+}
+
+const can4: CanType = new Can({
+    flavor: 'grapefruit',
+    ounces: 12
+})
+
+test('can1 and can2 are not the exact same instance', () => {
+    expect(can1).not.toBe(can2)
+})
+
+test('can2 and can3 are the same instance', () => {
+    expect(can2).toBe(can3)
+})
+
+test('can1 and can2 have the same properties', () => {
+    expect(can1).toEqual(can2)
+})
+
+test('can2 and can4 have the same properties', () => {
+    expect(can2).toEqual(can4)
+})
+
+test('can2 and can4 are differenct class', () => {
+    expect(can2).not.toStrictEqual(can4)
+})
+
+test('differences between toEqual and toStrictEqual', () => {
+    expect({foo: NaN, bar: undefined}).toEqual({foo: NaN})
+
+    expect({foo: NaN, bar: undefined}).not.toStrictEqual({foo: NaN})
+
+    expect([, undefined, 1]).toEqual([undefined, , 1])
+
+    expect([, undefined, 1]).not.toStrictEqual([undefined, , 1])
+})

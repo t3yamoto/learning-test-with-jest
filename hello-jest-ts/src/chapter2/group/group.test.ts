67ea42b84e958058a97f02244d2370ffa5a6368b
t3yamoto <3yamoto.dev@gmail.com>
2023-04-22 23:03:53 +0900

    2.4.3

--- hello-jest-ts/src/chapter2/group/group.test.ts
+++ hello-jest-ts/src/chapter2/group/group.test.ts
@@ -42,3 +42,28 @@ test.each([
 ])('calculates the sales tax for a price equal to $price', ({ price, expected }) => {
     expect(calculateSalesTax(price)).toBe(expected)
 })
+
+// 2.4.3 前後処理を設定
+
+// グループ1
+describe('before/after timing', () => {
+    // グループ1の前後処理
+    beforeAll(() => console.log('1 - beforeAll'))
+    afterAll(() => console.log('1 - afterAll'))
+    beforeEach(() => console.log('1 - beforeEach'))
+    afterEach(() => console.log('1 - afterEach'))
+    // グループ1のテスト
+    test('', () => console.log('1 - test1'))
+    // グループ2
+    describe('Scoped / Nested block', () => {
+        // グループ2の前後処理
+        beforeAll(() => console.log('2 - beforeAll'))
+        afterAll(() => console.log('2 - afterAll'))
+        beforeEach(() => console.log('2 - beforeEach'))
+        afterEach(() => console.log('2 - afterEach'))
+        // グループ2のテスト1
+        test('', () => console.log('2 - test1'))
+        // グループ2のテスト2
+        test('', () => console.log('2 - test2'))
+    })
+})

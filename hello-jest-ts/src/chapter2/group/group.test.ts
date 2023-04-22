372f234336d0e3362c31cf8461a3d407424f556e
t3yamoto <3yamoto.dev@gmail.com>
2023-04-22 22:36:24 +0900

    2.4.2

--- hello-jest-ts/src/chapter2/group/group.test.ts
+++ hello-jest-ts/src/chapter2/group/group.test.ts
@@ -19,3 +19,26 @@ describe('グループ名', () => {
         })
     })
 })
+
+// 2.4.2 パラメタライズドテスト
+
+test.each([
+    {a: 1, b: 1, expected: 2},
+    {a: 1, b: 2, expected: 3},
+    {a: 2, b: 1, expected: 3},
+])('.add($a, $b)', ({ a, b, expected }) => {
+    expect(a + b).toBe(expected)
+})
+
+const calculateSalesTax = (price: number) =>
+    price > 0 ? Math.floor((price / 100) * 10) : 0
+
+test.each([
+    {price: 100, expected: 10},
+    {price: 99, expected: 9},
+    {price: 1, expected: 0},
+    {price: 0.1, expected: 0},
+    {price: 0, expected: 0},
+])('calculates the sales tax for a price equal to $price', ({ price, expected }) => {
+    expect(calculateSalesTax(price)).toBe(expected)
+})

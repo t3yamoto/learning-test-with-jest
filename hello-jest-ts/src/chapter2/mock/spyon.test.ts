073e990b39b6207b524c6f63b43974683a386986
t3yamoto <3yamoto.dev@gmail.com>
2023-04-30 11:47:22 +0900

    2.6.3

---
+++ hello-jest-ts/src/chapter2/mock/spyon.test.ts
@@ -0,0 +1,17 @@
+
+describe('Math.random with spyOn', () => {
+    let spy
+
+    afterEach(() => {
+        spy.mockRestore()
+    })
+
+    it('Math.random return 1', () => {
+        spy = jest.spyOn(Math, 'random').mockImplementation(() => 1)
+        expect(Math.random()).toBe(1)
+    })
+
+    it('Math.random return under 1', () => {
+        expect(Math.random()).toBeLessThan(1)
+    })
+})
